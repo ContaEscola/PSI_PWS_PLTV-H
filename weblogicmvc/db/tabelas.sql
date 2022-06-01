2819ac0850689628a1349e116ea6bfbc9fab0022
ContaEscola <2211904@my.ipleiria.pt>
2022-06-01 22:47:19 +0100

    PPH-42 /Tabelas script atualizado totalmente
    O script das tabelas está completo agora.

--- weblogicmvc/db/tabelas.sql
+++ weblogicmvc/db/tabelas.sql
@@ -1,4 +1,6 @@
+CREATE DATABASE pws_h;
 
+USE pws_h;
 
 /*
  Iremos utilizar o PASSWORD_DEFAULT como algoritmo de hash logo é recomendado 255 characters na password
@@ -18,6 +20,7 @@ role 				ENUM('Administrador','Funcionário','Cliente')  	DEFAULT 'Cliente',
 CONSTRAINT pk_user 	PRIMARY KEY(id)
 ) ENGINE = InnoDB;
 
+
 CREATE TABLE empresas(
 id 						INT 				UNSIGNED	AUTO_INCREMENT, 
 designacaoSocial 		VARCHAR(30) 		NOT NULL,
@@ -25,62 +28,58 @@ email 					VARCHAR(150) 		NOT NULL,
 telefone 				CHAR(9) 			NOT NULL,
 nif 					CHAR(9) 			NOT NULL,
 morada 					VARCHAR(255) 		NOT NULL,
-capitalSocial 			INT 				NOT NULL,
+capitalSocial 			DOUBLE				NOT NULL,
 localidade  			VARCHAR(50) 		NOT NULL,
 codPostal 				CHAR(7) 			NOT NULL,
-CONSTRAINT pk_empresa	PRIMARY KEY (id)
+CONSTRAINT pk_empresa	PRIMARY KEY(id)
 ) ENGINE = InnoDB;
 
 
-/*
-CREATE TABLE faturas (
-  idfatura  int unsigned AUTO_INCREMENT ,
-  data date NOT NULL,
-  valortotal  double NOT NULL,
-  ivatotal int(11) NOT NULL,
-  estado varchar(25) NOT NULL,
-  referenciafunc int(11) NOT NULL,
-  referenciacliente  int(11) NOT NULL,
-  CONSTRAINT pk_faturas PRIMARY KEY (idfatura),
-   constraint fk_faturas_referenciafunc foreign key users (referenciafunc)
-references users (iduser),
- constraint fk_faturas_referenciacliente foreign key  users (referenciacliente)
- references users (iduser)
-  
-) ENGINE=InnoDB;
-
-
-
-
 CREATE TABLE ivas (
-  idiva int unsigned auto_increment,
-  percentagem  int(11) NOT NULL,
-  descricao varchar(35) NOT NULL,
-  emvigor  int(11) NOT NULL,
-  CONSTRAINT pk_ivas PRIMARY KEY (idiva) 
-) ENGINE=InnoDB;
+id 					INT 				UNSIGNED	AUTO_INCREMENT,
+percentagem  		INT 				UNSIGNED 	NOT NULL,
+descricao 			VARCHAR(35) 		NOT NULL,
+emVigor  			BOOL 				NOT NULL,
+CONSTRAINT pk_iva	PRIMARY KEY(id) 
+) ENGINE = InnoDB;
 
-
-
-CREATE TABLE linhafaturas (
-  idlinhaf  int unsigned auto_increment,
-  quantidade int(11) NOT NULL,
-  valorunitario int(11) NOT NULL,
-  valoriva  int(11) NOT NULL,
-  referenciafatura  int(11) NOT NULL,
-  referenciaproduto  int(11) NOT NULL,
-  CONSTRAINT pk_linhafaturas PRIMARY KEY (idlinhaf)
-  
-) ENGINE=InnoDB;
-
-
-
+-- A referência tem 7 caracteres, exemplo de uma referencia: 01.0001
 CREATE TABLE produtos (
-  idproduto  int unsigned auto_increment,
-   referencia  int(11) NOT NULL,
-  descricao  varchar(35) NOT NULL,
-  preco  double NOT NULL,
-  sotck  int(11) NOT NULL,
-   CONSTRAINT pk_produtos PRIMARY KEY (idproduto) 
-) ENGINE=InnoDB;
+id  						INT 					UNSIGNED				AUTO_INCREMENT,
+referencia  				CHAR(7) 				NOT NULL,
+descricao  					VARCHAR(50)				NOT NULL,
+preco  						DOUBLE 					NOT NULL,
+stock  						INT						NOT NULL,
+iva_id						INT 					UNSIGNED				NOT NULL,
+CONSTRAINT pk_produto		PRIMARY KEY(id),
+CONSTRAINT fk_produto_iva	FOREIGN KEY(iva_id)		REFERENCES ivas(id)
+) ENGINE = InnoDB;
 
+
+-- O valor total aceita até 9 digitos com 3 casas decimais assim o máximo será 999.999,999
+-- O valor do iva aceita até 7 digitos com 3 casas decimais assim o máximo será 99.999,999
+CREATE TABLE faturas (
+id  					INT 								UNSIGNED	AUTO_INCREMENT,
+data 					DATETIME 							NOT NULL,
+valorTotal 				DECIMAL(9,3) 						NOT NULL,
+ivaTotal 				DECIMAL(7,3) 						NOT NULL,
+estado 					ENUM('Em Lançamento', 'Emitida')	NOT NULL,
+referenciaFuncionario	INT 								UNSIGNED	NOT NULL,
+referenciaCliente  		INT 								UNSIGNED 	NOT NULL,
+CONSTRAINT pk_fatura	PRIMARY KEY(id)
+) ENGINE = InnoDB;
+
+
+-- O valor unitário aceita até 7 digitos com 3 casas decimais assim o máximo será 9.999,999
+-- O valor do iva aceita até 6 digitos com 3 casas decimais assim o máximo será 999,999
+CREATE TABLE linhaFaturas (
+id  								INT 						UNSIGNED					AUTO_INCREMENT,
+quantidade 							INT 						UNSIGNED 					NOT NULL,
+valorUnitario						DECIMAL(7,3) 				NOT NULL,
+valorIva  							DECIMAL(6,3) 				NOT NULL,
+fatura_id 							INT 						UNSIGNED					NOT NULL,
+produto_id							INT 						UNSIGNED 					NOT NULL,
+CONSTRAINT pk_linhaFatura			PRIMARY KEY(id),
+CONSTRAINT fk_linhaFatura_fatura	FOREIGN KEY(fatura_id)		REFERENCES faturas(id),
+CONSTRAINT fk_linhaFatura_produto	FOREIGN KEY(produto_id)		REFERENCES produtos(id)
+) ENGINE = InnoDB;
