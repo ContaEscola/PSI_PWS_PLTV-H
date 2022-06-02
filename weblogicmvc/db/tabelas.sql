a1b9e6493886ff07d349c236bcbd50eb1cd17bbb
ContaEscola <92809778+ContaEscola@users.noreply.github.com>
2022-06-02 12:16:46 +0100

    Pph 42 criacao da base de dados (#6)
    * PPH-42 /DER Inicial da Filipa
    
    Este é o DER inicial da Filipa
    
    * PPH-42 /DER completo
    
    Faltava algumas coisas ao DER da Filipa por isso foi necessário atualizar.
    
    * PPH-42 /Script inicial da Filipa
    
    * PPH-42 /Sql atualizado parcialmente
    
    O script foi renomeado para tabelas para organizar, e foi atualizado parcialmente.
    
    * PPH-42 /Tabelas script atualizado totalmente
    
    O script das tabelas está completo agora.
    
    * PPH-42 /Adicionei as unique keys às tabelas
    
    No commit anterior esqueci-me das unique keys logo adicionei-as neste.
    
    * PPH-42 /Fix nas unique keys
    
    As unique keys estavam mal definidas
    
    * DER Revertido à versão anterior
    
    * PPH-42 /Mudança no limite
    
    Mudei o limite do campo descrição da tabela produtos de 50 para 255.
    
    * PPH-42 /Mudei os campos com tipo decimal
    
    Mudei todos os campos de decimal para double já que os produtos utilizam double.
    
    * PPH-42 /Script com os registos parcialmente feito
    
    Ainda falta acabar as faturas e as linhasFaturas
    
    * PPH-42 /Scrip registos totalmente feito
    
    * PPH-42 /Modifiquei nome da tabela linhaFaturas
    
    Modifiquei o nome de linhaFaturas para linhafaturas
    
    * PPH-42 /Adicionei o scrip com os vários utilizadores(perfis)

---
+++ weblogicmvc/db/tabelas.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE pws_h;
+
+USE pws_h;
+
+/*
+ Iremos utilizar o PASSWORD_DEFAULT como algoritmo de hash logo é recomendado 255 characters na password
+ https://www.php.net/manual/en/function.password-hash.php 
+*/
+CREATE TABLE users(
+id 								INT 											UNSIGNED 	AUTO_INCREMENT,
+username 						VARCHAR(50) 									NOT NULL,
+password 						VARCHAR(255)									NOT NULL,
+email 							VARCHAR(150) 									NOT NULL,
+telefone 						CHAR(9) 										NOT NULL,
+nif 							CHAR(9) 										NOT NULL,
+morada 							VARCHAR(255) 									NOT NULL,
+codPostal 						CHAR(8) 										NOT NULL,
+localidade 						VARCHAR(50) 									NOT NULL,
+role 							ENUM('Administrador','Funcionário','Cliente')  	DEFAULT 'Cliente',
+CONSTRAINT pk_user 				PRIMARY KEY(id),
+CONSTRAINT uk_user_username 	UNIQUE(username),
+CONSTRAINT uk_user_nif 			UNIQUE(nif)
+) ENGINE = InnoDB;
+
+
+CREATE TABLE empresas(
+id 											INT 						UNSIGNED	AUTO_INCREMENT, 
+designacaoSocial 							VARCHAR(30) 				NOT NULL,
+email 										VARCHAR(150) 				NOT NULL,
+telefone 									CHAR(9) 					NOT NULL,
+nif 										CHAR(9) 					NOT NULL,
+morada 										VARCHAR(255) 				NOT NULL,
+capitalSocial 								DOUBLE						NOT NULL,
+localidade  								VARCHAR(50) 				NOT NULL,
+codPostal 									CHAR(8) 					NOT NULL,
+CONSTRAINT pk_empresa						PRIMARY KEY(id),
+CONSTRAINT uk_empresa_designacaoSocial		UNIQUE(designacaoSocial),
+CONSTRAINT uk_empresa_email 				UNIQUE(email),
+CONSTRAINT uk_empresa_telefone				UNIQUE(telefone),
+CONSTRAINT uk_empresa_nif					UNIQUE(nif)
+) ENGINE = InnoDB;
+
+
+CREATE TABLE ivas (
+id 					INT 				UNSIGNED	AUTO_INCREMENT,
+percentagem  		INT 				UNSIGNED 	NOT NULL,
+descricao 			VARCHAR(35) 		NOT NULL,
+emVigor  			BOOL 				NOT NULL,
+CONSTRAINT pk_iva	PRIMARY KEY(id),
+CONSTRAINT uk_iva	UNIQUE(descricao) 
+) ENGINE = InnoDB;
+
+-- A referência tem 7 caracteres, exemplo de uma referencia: 01.0001
+CREATE TABLE produtos (
+id  								INT 					UNSIGNED				AUTO_INCREMENT,
+referencia  						CHAR(7) 				NOT NULL,
+descricao  							VARCHAR(255)			NOT NULL,
+preco  								DOUBLE 					NOT NULL,
+stock  								INT						NOT NULL,
+iva_id								INT 					UNSIGNED				NOT NULL,
+CONSTRAINT pk_produto				PRIMARY KEY(id),
+CONSTRAINT uk_produto_referencia	UNIQUE(referencia),
+CONSTRAINT uk_produto_descricao		UNIQUE(descricao),
+CONSTRAINT fk_produto_iva			FOREIGN KEY(iva_id)		REFERENCES ivas(id)
+) ENGINE = InnoDB;
+
+
+
+CREATE TABLE faturas (
+id  					INT 								UNSIGNED	AUTO_INCREMENT,
+data 					DATETIME 							NOT NULL,
+valorTotal 				DOUBLE								NOT NULL,
+ivaTotal 				DOUBLE 								NOT NULL,
+estado 					ENUM('Em Lançamento', 'Emitida')	NOT NULL,
+referenciaFuncionario	INT 								UNSIGNED	NOT NULL,
+referenciaCliente  		INT 								UNSIGNED 	NOT NULL,
+CONSTRAINT pk_fatura	PRIMARY KEY(id)
+) ENGINE = InnoDB;
+
+
+
+CREATE TABLE linhafaturas (
+id  								INT 						UNSIGNED					AUTO_INCREMENT,
+quantidade 							INT 						UNSIGNED 					NOT NULL,
+valorUnitario						DOUBLE 						NOT NULL,
+valorIva  							DOUBLE		 				NOT NULL,
+fatura_id 							INT 						UNSIGNED					NOT NULL,
+produto_id							INT 						UNSIGNED 					NOT NULL,
+CONSTRAINT pk_linhaFatura			PRIMARY KEY(id),
+CONSTRAINT fk_linhaFatura_fatura	FOREIGN KEY(fatura_id)		REFERENCES faturas(id),
+CONSTRAINT fk_linhaFatura_produto	FOREIGN KEY(produto_id)		REFERENCES produtos(id)
+) ENGINE = InnoDB;
