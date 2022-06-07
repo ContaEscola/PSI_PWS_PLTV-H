c473c7d0b2901aa4c17f4777aad61fac5ee51f02
Tomas Burgos <tomassousa99@gmail.com>
2022-06-07 11:19:42 +0100

    Landing Page e alterei o css

---
+++ weblogicmvc/app/views/LandingPage/LandingPage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/fg.css">
+
+</head>
+
+<body class="[ body ] [ grid ]">
+<header class="[ header ] [ flex ]">
+    <div class="grid g-place-items-center">
+        <img src="img/Logo.png" alt="">
+    </div>
+    <nav class="margin-left-auto">
+        <ul class="[ nav__list ] [ flex ]">
+            <li><a href="#" class="button" data-type="primary">Login</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main class="[ main-content ] [ flow ]" data-type="how-to-start">
+    <h1 class="[ how-to-start__title ] [ text-align-center ] [ fs-600 ] ">Deixe as suas faturas connosco!</h1>
+        <p class="[ fs-400 ]">Cansado de ter as suas faturas desorganizadas e em molhos, deixe connosco.
+        <br>
+            Nós guardamos-as e organizamos-as por si!</p>
+    <br>
+    <a href="#" class="button" data-type="outline">Começar</a>
+
+</main>
+
+</body>
+
+</html>
