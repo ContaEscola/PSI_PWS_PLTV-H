d1c62658cba6336c05f7b264b45d045549cdbc60
Tomas Burgos <tomassousa99@gmail.com>
2022-06-13 23:17:42 +0100

    Pph 40 desktop landing page (#13)
    * Landing Page e alterei o css
    
    * Criei a Landing Page e alterei o css
    
    * PPH-41 //  Alterei a landingpage
    
    * Fix dos css e página feita corretamente
    
    Co-authored-by: ContaEscola <2211904@my.ipleiria.pt>

---
+++ views/LandingPage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt"><head>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body class="[ body ] [ grid ]">
+<header class="[ header ] [ flex ]">
+    <div class="grid g-place-items-center">
+        <img src="" alt="logo">
+    </div>
+    <nav class="margin-left-auto">
+        <ul class="[ nav__list ] [ flex ]">
+            <li><a href="" class="button" data-type="primary">Login</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main class="[ main-content ] [ flow ]" data-type="how-to-start">
+    <h1 class="[ how-to-start__title ] [ text-align-center ] [ fs-600 ] ">Deixe as suas faturas connosco!</h1>
+    <p class="[ fs-400 ]">Cansado de ter as suas faturas desorganizadas e em molhos, deixe connosco.
+        <br>
+        Nós guardamos-as e organizamos-as por si!</p>
+    <br>
+    <a href="" class="button" data-type="outline">Começar</a>
+
+</main>
+
+
+
+</body></html>
