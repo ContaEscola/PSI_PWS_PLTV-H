4c4856f02c44412507027be7e8d43860bd22ca64
filipa234 <73118957+filipa234@users.noreply.github.com>
2022-06-15 15:07:17 +0100

    modelos da base de dados  (feitos)
    modelos da base de dados feitos

--- models/Fatura.php
+++ models/Fatura.php
@@ -3,8 +3,22 @@ use \ActiveRecord\Model;
 
 class Fatura extends Model
 {
+    static $table_name = 'faturas';
+    static $primary_key = 'id';
+
+    static $connection = 'cliente';
+
     static $validates_presence_of = array(
         array('id'),
-        array('data', 'message' => 'YooaaH it must be provided')
+        array('data', 'message' => 'insira a data da fatura ')
     );
+
+
+    static $validates_size_of = array(
+        array('title', 'within' => array(1,5), 'too_short' => 'too short!'),
+        array('cover_blurb', 'is' => 20),
+        array('data', 'maximum' => 10, 'too_long' => 'data da fatura ')
+    );
+
+
 }
--- models/Produto.php
+++ models/Produto.php
@@ -3,9 +3,25 @@ use \ActiveRecord\Model;
 
 class Produto  extends Model
 {
+
+    static $table_name = 'produtos';
+    static $primary_key = 'id';
+
+    static $connection = 'cliente';
+
     static $validates_presence_of = array(
         array('id'),
-        array('referencia', 'message' => 'YooaaH it must be provided')
+        array('descricao', 'message' => 'insira a descricao do produto ')
     );
+
+    static $validates_size_of = array(
+           array('title', 'within' => array(1,5), 'too_short' => 'too short!'),
+      array('cover_blurb', 'is' => 20),
+      array('descricao', 'maximum' => 10, 'too_long' => 'descricao do produto')
+    );
+
+
+
+
 
 }
