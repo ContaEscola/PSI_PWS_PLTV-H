a557b48ed76172d0287369b78c84a12d0a118b45
Tomas Burgos <tomassousa99@gmail.com>
2022-06-15 11:08:10 +0100

    Pph 49 site controller (#15)
    * PPH-44 /Página parcialmente feita
    
    * PPH-44 /Página feita
    
    * PPH-44 /Adicionei pasta mãe
    
    A pasta mãe passou a ser loggedIn em vez de views
    
    * PPH-45 /Página feita
    
    * PPH-46 /Página feita
    
    * PPH-47 /Página parcialmente feita
    
    * PPH-47 /Página quase acabada
    
    Só falta inserir o nome dos links corretos.
    
    * PPH-47 /Página feita
    
    * PPH-48 /Modifiquei a aparência dos inputs
    
    * Create boneco.png
    
    * PPH-48 /Modifiquei a aparência do input
    
    * PPH-49 SiteController
    
    Nao esta concluido.
    
    * PPH-47 /Página feita
    
    * PPH-48 /Parte da página feita
    
    * PPH-48 /Outra parte da página feita
    
    * Fixes dos dropdowns
    
    * PPH-48 /Fix na página das faturas do cliente
    
    * PPH-48 /Página feita
    
    * PPH-48 /Fixes nas tabelas
    
    * Final update
    
    * Parte do fix
    
    * Fixes no controller
    
    Co-authored-by: Pedro Norberto <2211904@my.ipleiria.pt>

--- weblogicmvc/app/views/home/howToStart.php
+++ weblogicmvc/app/views/home/howToStart.php
@@ -5,25 +5,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Como começar</title>
+    <title>Como começar ?</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="../../../public/css/style.css">
+    <link rel="stylesheet" href=".//css/style.css">
 
 </head>
 
 <body class="[ body ] [ grid ]">
     <header class="[ header ] [ flex ]">
         <div class="grid g-place-items-center">
-            <img src="../../../public/assets/logo.png" alt="">
+            <img src="./assets/logo.png" alt="">
         </div>
-        <nav class="margin-left-auto">
+        <nav class="flex margin-left-auto align-items-center">
             <ul class="[ nav__list ] [ flex ]">
-                <li><a href="#" class="button" data-type="primary">Home</a></li>
-                <li><a href="#" class="button" data-type="outline">Login</a></li>
+                <li><a href="./?c=Home&a=index" class="button" data-type="primary">Home</a></li>
+                <li><a href="./?c=Login&a=index" class="button" data-type="outline">Login</a></li>
             </ul>
         </nav>
     </header>
@@ -42,9 +42,9 @@
         <div>
             <h2 class="fs-500 fw-bold">2. Efetuar o Login</h2>
             <div class="margin-top-2">
-                <p>Assim que tiver acesso à informação sobre a conta, só precisa de efetuar o <a href="#">Login</a>.</p>
+                <p>Assim que tiver acesso à informação sobre a conta, só precisa de efetuar o <a href="./?c=Login&a=index">Login</a>.</p>
                 <p>
-                    Depois de efetuar o <a href="#">Login</a>, terá acesso total ao serviço, que inclui, gerir as suas
+                    Depois de efetuar o <a href="./?c=Login&a=index">Login</a>, terá acesso total ao serviço, que inclui, gerir as suas
                     faturas, etc.
                 </p>
             </div>
