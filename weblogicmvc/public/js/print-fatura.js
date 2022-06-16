30654de1607b03f36c2291cd3919a8cf6158bb94
ContaEscola <2211904@my.ipleiria.pt>
2022-06-16 20:24:13 +0100

    Rework no print da fatura

--- weblogicmvc/public/js/print-fatura.js
+++ weblogicmvc/public/js/print-fatura.js
@@ -1,13 +1,19 @@
-function printDiv() {
-    const originalContents = document.body.innerHTML;
-    const printContents = document.getElementById('printFatura');
-    const container = printContents.querySelector(".fatura-container__subcontainer");
-    const lista = printContents.querySelector(".fatura__products-list");
-    container.classList.remove('fatura-container__subcontainer');
-    lista.style.width = "unset";
-    const print = printContents.innerHTML;
+const printBtn = document.querySelector('[data-print-receipt]');
 
-    document.body.innerHTML = print;
+printBtn.addEventListener('click', () => {
+    let originalPage = document.body.innerHTML;
+    let receipt = document.getElementById('fatura');
+
+    let receiptContainer = receipt.querySelector(".fatura-container__subcontainer");
+    let productsList = receipt.querySelector(".fatura__products-list");
+
+    receiptContainer.classList.remove('fatura-container__subcontainer');
+    productsList.style.width = "unset";
+
+    let receiptReadyToPrint = receipt.innerHTML;
+
+    document.body.innerHTML = receiptReadyToPrint;
     window.print();
-    document.body.innerHTML = originalContents;
-}
+    document.body.innerHTML = originalPage;
+})
+
